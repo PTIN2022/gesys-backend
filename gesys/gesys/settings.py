a2cace545c63ccb66a60fc56f19432dec99912c8
Abdullah <aabdullahsmx2014@gmail.com>
2022-03-27 17:40:15 +0200

    Add permission classes

--- gesys/gesys/settings.py
+++ gesys/gesys/settings.py
@@ -129,5 +129,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
